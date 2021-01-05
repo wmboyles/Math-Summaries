f0c6239680bbfdfeced58df22933dea2a7c5175d
William Boyles <williamboyles22900@gmail.com>
2021-01-05 16:31:44 -0500

    Credit contributors with script.
    Relevant to #3, I have added a file called contributors.tex that thanks
    everyone who has contributed to the project on Github. contributors.tex
    is generated by make_contributors.py. It uses the GitHub REST API to
    get each contributor's name and username on Github.
    
    In the future, I hope to have this contributors.py script automate
    updating the contributors.tex file. Ideally, after each push/merge into
    the master branch, the contributors.py script would run and update
    contributors.tex.
    
    It'd be nice if this could be tied into another action like the
    [tectonic typesetting
    system](https://tectonic-typesetting.github.io/en-US/), which would
    allow the creation of PDFs to be automated. This would allow #6 to be
    solved in a much more elegant way if PDFs could still be gitignored but
    also generated for everyone to see.

---
+++ make_contributors.py
@@ -0,0 +1,58 @@
+"""
+Queries GitHub API to list contributors and their names.
+Uses this info to generate the contributors page.
+
+author: William Boyles (wmboyles)
+"""
+
+import requests
+
+# Get contributor info, specifically username (login) and name
+res = requests.get("https://api.github.com/repos/wmboyles/Math-Summaries/contributors")
+
+if not res.ok:
+    raise Exception(f"ERROR - {res.status_code}: {res.text}")
+
+contributors = res.json()
+for contributor in contributors:
+    res = requests.get(contributor['url'])
+
+    # Default to username if request fails or no full name
+    if not res.ok:
+        contributor['name'] = contributor['login']
+        continue
+
+    contributor_info = res.json()
+    if 'name' not in contributor_info or contributor_info['name'] is None:
+        contributor['name'] = contributor['login']
+        continue
+
+    contributor['name'] = contributor_info['name']
+
+
+# Create the contributors.tex page
+with open("contributors.tex", 'w') as f:
+    f.write("""\section{Contributors}
+Special thanks to everyone who made contributions to this project on \href{https://github.com/wmboyles/Math-Summaries}{Github}.
+They are listed in order of number of commits as \\texttt{name (GitHub username)}.
+
+\\begin{center}
+    \\begin{tabular}{ c c c }
+    """)
+
+    i = 0
+    for contributor in contributors:
+        if i % 3 != 0:
+            f.write(" & ")
+        else:
+            f.write("\t")
+        
+        f.write(f"{contributor['name']} (\\href{{{contributor['url']}}}{{{contributor['login']}}})")
+
+        if i % 3 == 2:
+            f.write(" \\\\\n\t")
+        
+        i += 1
+
+    f.write("""\\end{tabular}
+\\end{center}\n""")
